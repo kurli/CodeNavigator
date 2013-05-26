f0fbccd79515ddec2ed5e9c9e4fa3155c02964d1
Guozhen Li <lgz_software@localhost.(none)>
2013-05-27 01:24:32 +0800

    add untracked files

---
+++ HttpService/HttpService/HTTPService.m
@@ -0,0 +1,23 @@
+//
+//  HTTPService.m
+//  HTTPService
+//
+//  Created by Guozhen Li on 12/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HTTPService.h"
+
+@implementation HTTPService
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
---
+++ HttpService/HttpService/HttpService.h
@@ -0,0 +1,13 @@
+//
+//  HTTPService.h
+//  HTTPService
+//
+//  Created by Guozhen Li on 12/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTTPService : NSObject
+
+@end
