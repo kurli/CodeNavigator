1e9933312b3d1b9e63054f251cc37d8fe9367998
Guangzhen <guangzhen@hotmail.com>
2014-04-13 00:31:46 +0800

    highlight performance tuning

--- lgz_javascript.js
+++ lgz_javascript.js
@@ -16,29 +16,29 @@ function addResult(id, count)
     if (index == 0)
         smoothScroll(id);
     //for (i=0; i<count; i++)
-        returnArray[index++] = id;
+    returnArray[index++] = id;
 }
-function encode(s){ 
-    return  s.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/([\\\.\*\[\]\(\)\$\^])/g,"\\$1"); 
-} 
-function decode(s){ 
-    return  s.replace(/\\([\\\.\*\[\]\(\)\$\^])/g,"$1").replace(/&gt;/g,">").replace(/&lt;/g,"<").replace(/&amp;/g,"&"); 
-} 
-function highlight(s){ 
+function encode(s){
+    return  s.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/([\\\.\*\[\]\(\)\$\^])/g,"\\$1");
+}
+function decode(s){
+    return  s.replace(/\\([\\\.\*\[\]\(\)\$\^])/g,"$1").replace(/&gt;/g,">").replace(/&lt;/g,"<").replace(/&amp;/g,"&");
+}
+function highlight(s){
     returnArray.splice(0, returnArray.length);
     index = 0;
-    if (s.length==0){ 
+    if (s.length==0){
         return;
-    } 
-    s=encode(s); 
-    var obj=document.getElementsByTagName("tbody")[0]; 
-    var t=obj.innerHTML.replace(/<span\s+class=.?highlight.?>([^<>]*)<\/span>/gi,"$1"); 
-    obj.innerHTML=t; 
-    var cnt=loopSearch(s,obj); 
+    }
+    s=encode(s);
+    var obj=document.getElementsByTagName("tbody")[0];
+    var t=obj.innerHTML.replace(/<span\s+class=.?highlight.?>([^<>]*)<\/span>/gi,"$1");
+    obj.innerHTML=t;
+    var cnt=loopSearch(s,obj);
     t=obj.innerHTML
-    var r=/{searchHL}(({(?!\/searchHL})|[^{])*){\/searchHL}/g 
+    var r=/{searchHL}(({(?!\/searchHL})|[^{])*){\/searchHL}/g
     t=t.replace(r,"<span class='highlight'>$1</span>");
-    obj.innerHTML=t; 
+    obj.innerHTML=t;
     return cnt;
 }
 
@@ -46,7 +46,13 @@ function clearHighlight() {
     while (1) {
         var objs = document.getElementsByClassName('highlight');
         if (objs.length != 0) {
-            objs[0].className = 'other';
+            var childs = objs[0].parentNode.childNodes;
+            var i=0;
+            for (i=0; i<childs.length; i++) {
+                if (childs[i] == objs[0]){
+                    childs[i].outerHTML = objs[0].innerText;
+                }
+            }
         } else {
             break;
         }
@@ -54,13 +60,13 @@ function clearHighlight() {
 }
 
 function highlight_this_line_keyword(line, s){
-    s=encode(s); 
+    s=encode(s);
     var obj = document.getElementById(line);
-    var cnt=loopSearch(s,obj); 
-    t=obj.innerHTML 
-    var r=/{searchHL}(({(?!\/searchHL})|[^{])*){\/searchHL}/g 
+    var cnt=loopSearch(s,obj);
+    t=obj.innerHTML
+    var r=/{searchHL}(({(?!\/searchHL})|[^{])*){\/searchHL}/g
     t=t.replace(r,"<span class='highlight'>$1</span>");
-    obj.innerHTML=t; 
+    obj.innerHTML=t;
     return cnt;
 }
 
@@ -93,7 +99,7 @@ document.addEventListener('touchmove', function(event) {
 
 function currentYPosition() {
     // Firefox, Chrome, Opera, Safari
-    if (self.pageYOffset) 
+    if (self.pageYOffset)
         return self.pageYOffset;
     return 0;
 }
@@ -123,8 +129,8 @@ function highlight_keyword_by_lines(lines, s)
         obj = document.getElementById(lineID);
         if (returnVal == -1)
         {
-             if (curY <= elmYPosition(lineID))
-                 returnVal = i;
+            if (curY <= elmYPosition(lineID))
+                returnVal = i;
         }
     }
     return returnVal;
@@ -140,11 +146,11 @@ function smoothScrollToPosition(stopPosition)
     if (distance == 0)
         return;
     if (distance < 100) {
-        scrollTo(0, stopY); 
+        scrollTo(0, stopY);
         return;
     }
     if (distance > 1000){
-        scrollTo(0, stopY); 
+        scrollTo(0, stopY);
         return;
     }
     var speed = Math.round(distance / 1000);
@@ -175,8 +181,8 @@ function FocusLine(eID) {
 }
 
 function smoothScroll(eID) {
-//    var stopY = elmYPosition(eID) - 200;
-//    scrollTo(0, stopY);
+    //    var stopY = elmYPosition(eID) - 200;
+    //    scrollTo(0, stopY);
     var str = "#"+eID;
     if (window.location.hash == str)
     {
@@ -193,37 +199,37 @@ function gotoLine(i)
     smoothScroll(returnArray[i]);
 }
 
-function loopSearch(s,obj){ 
-    var cnt=0; 
+function loopSearch(s,obj){
+    var cnt=0;
     var temp = 0;
-    if (obj.nodeType==3){ 
-        cnt=replace(s,obj); 
-        return cnt; 
-    } 
+    if (obj.nodeType==3){
+        cnt=replace(s,obj);
+        return cnt;
+    }
     var id="";
-    for (var i=0,c;c=obj.childNodes[i];i++){ 
+    for (var i=0,c;c=obj.childNodes[i];i++){
         id = c.id;
-        if (!c.className||c.className!="highlight") 
+        if (!c.className||c.className!="highlight")
         {
-            temp =loopSearch(s,c); 
+            temp =loopSearch(s,c);
             if (temp>0 && id != undefined && id != "")
                 addResult(id, temp);
             cnt+=temp;
         }
-    } 
-    return cnt; 
-} 
-function replace(s,dest){ 
-    var r=new RegExp(s,"g"); 
-    var tm=null; 
-    var t=dest.nodeValue; 
-    var cnt=0; 
-    if (tm=t.match(r)){ 
-        cnt=tm.length; 
-        t=t.replace(r,"{searchHL}"+decode(s)+"{/searchHL}") 
-        dest.nodeValue=t; 
-    } 
-    return cnt; 
+    }
+    return cnt;
+}
+function replace(s,dest){
+    var r=new RegExp(s,"g");
+    var tm=null;
+    var t=dest.nodeValue;
+    var cnt=0;
+    if (tm=t.match(r)){
+        cnt=tm.length;
+        t=t.replace(r,"{searchHL}"+decode(s)+"{/searchHL}")
+        dest.nodeValue=t;
+    }
+    return cnt;
 }
 
 function mousedown(obj)
@@ -246,7 +252,7 @@ function showLines(start, end)
     var endID = 'L';
     endID = endID + end;
     
-    var tbody=document.getElementsByTagName("tbody")[0]; 
+    var tbody=document.getElementsByTagName("tbody")[0];
     var trList = tbody.getElementsByTagName("tr");
     
     var tagStartIndex;
@@ -275,8 +281,8 @@ function showLines(start, end)
     var i;
     for (i = tagStartIndex; i<=tagEndIndex; i++)
     {
-//        var id = 'L';
-//        id += i;
+        //        var id = 'L';
+        //        id += i;
         trList[i].style.display = 'table-row';
         //document.getElementById(id).style.display = 'table-row';
     }
@@ -287,7 +293,7 @@ function showLines(start, end)
     rowID += end;
     var rowElem = document.getElementById(rowID);
     
-    var tbody=document.getElementsByTagName("tbody")[0]; 
+    var tbody=document.getElementsByTagName("tbody")[0];
     tbody.removeChild(rowElem);
 }
 
@@ -297,7 +303,7 @@ function hideLines(token, start,end)
     startID = startID + start;
     var endID = 'L';
     endID = endID + end;
-    var tbody=document.getElementsByTagName("tbody")[0]; 
+    var tbody=document.getElementsByTagName("tbody")[0];
     var trList = tbody.getElementsByTagName("tr");
     
     var tagStartIndex = start;
@@ -326,9 +332,9 @@ function hideLines(token, start,end)
     var i;
     for (i = tagStartIndex; i<=tagEndIndex; i++)
     {
-//        var id = 'L';
-//        id += i;
-//        document.getElementById(id).style.display = 'none';
+        //        var id = 'L';
+        //        id += i;
+        //        document.getElementById(id).style.display = 'none';
         trList[i].style.display = 'none';
     }
     
@@ -336,7 +342,7 @@ function hideLines(token, start,end)
     id += end;
     var element = document.getElementById(id);
     
-    var tbody=document.getElementsByTagName("tbody")[0]; 
+    var tbody=document.getElementsByTagName("tbody")[0];
     var row = document.createElement("tr");
     var rowID = '';
     rowID +=start;
@@ -354,12 +360,12 @@ function hideLines(token, start,end)
     var startTD = startContent.childNodes[2];
     var content = startTD.textContent;
     i = content.search(token);
-    var subStr = content.substring(0, i);    
+    var subStr = content.substring(0, i);
     subStr += "{......}";
     var textNode = document.createTextNode(subStr);
     var divNode = document.createElement("div");
     divNode.appendChild(textNode);
-
+    
     divNode.setAttribute("onclick","showLines("+start+",'"+end +"')");
     th2.setAttribute("onclick","showLines("+start+",'"+end +"')");
     
@@ -388,9 +394,9 @@ function showComment(trID, comment)
     var row = document.createElement("tr");
     row.id = trID+"-comment";
     var th1 = document.createElement("th");
-//    //th1.appendChild(document.createTextNode(" "));
+    //    //th1.appendChild(document.createTextNode(" "));
     var th2 = document.createElement("th");
-//    //th2.appendChild(document.createTextNode(" "));
+    //    //th2.appendChild(document.createTextNode(" "));
     var td1 = document.createElement("td");
     comment = comment.replace(/lgz_br_lgz/ig, '\n');
     var textNode = document.createTextNode(comment);
